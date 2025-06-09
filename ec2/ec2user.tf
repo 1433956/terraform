865f9fa26ddc613577a0d83187ba5c2c17aed655
raghu <raghumurukutla96@gmail.com>
2025-06-09 23:26:23 +0530

    creating new scurity groups

--- ec2/ec2user.tf
+++ ec2/ec2user.tf
@@ -1,3 +1,14 @@
+resource "aws_instance" "expese" {
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    vpc_security_group_ids =[ aws_security_group.allow_all.id ]
+
+    tags = {
+
+        Name="mysql"
+    }
+  
+}
 resource "aws_security_group" "allow_all" {
 
     name="allow_all"
